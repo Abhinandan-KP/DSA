28c143651f8e88dfb320f773de3f6abb39eb770e
Abhinandan-KP <abhirop538@gmail.com>
2025-12-30 00:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (13.46%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-     int func(int i,int j,vector<vector<int>>&dp,vector<vector<int>>& grid)
-  {   
-      if(i==0 && j==0)
-      return grid[0][0];
-      
-      if(i<0 || j<0)
-      return INT_MAX;
-      
-      if(dp[i][j]!=-1)
-      return dp[i][j];
-      int uppath=func(i-1,j,dp,grid);
-      int leftpath=func(i,j-1,dp,grid);
-        int up=INT_MAX;
-        int left=INT_MAX;
-       
-       if(uppath!=INT_MAX) 
-      {up=grid[i][j]+ uppath;}
-      if(leftpath!=INT_MAX)
-      {left=grid[i][j]+leftpath;}
-     
-       return dp[i][j] = min(up,left);
-      }
-    int minPathSum(vector<vector<int>>& grid) {
-           int m=grid.size();
-       int n=grid[0].size();
-      vector<vector<int>>dp(m,vector<int>(n,-1));
-      return func(m-1,n-1,dp,grid);
+    int minPathSum(vector<vector<int>>& matrix) {
+         int m = matrix.size();
+    int n = matrix[0].size();
+       vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    dp[0][0] = matrix[0][0];
+                }
+                else {
+                    int up = matrix[i][j];
+                    if (i > 0)
+                        up += dp[i - 1][j];
+                    else
+                        up += 1e9;
+
+                    int left = matrix[i][j];
+                    if (j > 0)
+                        left += dp[i][j - 1];
+                    else
+                        left += 1e9;
+
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
