9d8d4b2a1fdeaa70c382aa1a52a2eee7330753b9
Abhinandan-KP <abhirop538@gmail.com>
2026-01-06 15:58:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triangle Path Sum/triangle-path-sum.cpp
+++ Difficulty: Medium/Triangle Path Sum/triangle-path-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
   public:
-    
-  
-    int fun(int i,int j,vector<vector<int>>&triangle,int n,vector<vector<int>>&dp)
-    {
-         if(dp[i][j]!=-1)
-         return dp[i][j];
-         
-         if(i==n-1)
-         {
-            return triangle[i][j];
-         }
-         int down=triangle[i][j]+fun(i+1,j,triangle,n,dp);
-         int dig=triangle[i][j]+fun(i+1,j+1,triangle,n,dp);
-         return dp[i][j]=min(down,dig);
-    }
- int minPathSum(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        // int m=triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return fun(0,0,triangle,n,dp);
+    int minPathSum(vector<vector<int>>& triangle) {
+        // Code here
+         int n=triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down=triangle[i][j]+dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+                
+              dp[i][j]=min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 
-    
 };
