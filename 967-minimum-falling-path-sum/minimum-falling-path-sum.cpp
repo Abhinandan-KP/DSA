ec9b4408f6d9a5b33f1dc1cd0c6c339e8d76501b
Abhinandan-KP <abhirop538@gmail.com>
2026-01-08 06:36:26 +0530

    Time: 3 ms (34.97%) | Memory: 15 MB (60.00%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,24 +3,21 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
           int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        for(int i=0;i<m;i++)
-        {
-            dp[n-1][i]=matrix[n-1][i];
-        }
-        for(int row=n-2;row>=0;row--)
-        {
-            for(int col=m-1;col>=0;col--)
+
+        vector<int>dp(matrix[n-1]);
+
+        for(int i=n-2;i>=0;i--)
+        {    vector<int>curr(m,0);
+            for(int j=0;j<m;j++)
             {
-                int down=dp[row+1][col];
-                int downleft=(col> 0)?dp[row+1][col-1]:1e9;
-                int downright=(col<m-1)?dp[row+1][col+1]:1e9;
-                
-                dp[row][col]=matrix[row][col]+min({down,downright,downleft});
+                int down=dp[j];
+                int  downleft=j>0?dp[j-1]:1e9;
+                int downright=j<m-1?dp[j+1]:1e9;
+
+                curr[j]=matrix[i][j]+min({down,downleft,downright});
             }
+            dp=curr;
         }
-        
-        int minsum= *min_element(dp[0].begin(),dp[0].end());
-        return minsum;
+        return *min_element(dp.begin(),dp.end());
     }
 };
