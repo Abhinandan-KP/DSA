d1f959f3df62efee3f86a75a7ae5188344818a1f
Abhinandan-KP <abhirop538@gmail.com>
2026-01-23 07:36:20 +0530

    Time: 41 ms (29.66%) | Memory: 60.6 MB (17.09%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int change(int T, vector<int>& arr) {
+        int n=arr.size();
+        if(n==0)
+        return T==0?1:0;
+
+        vector<unsigned long long> prev(T + 1, 0);  
+    for (int i = 0; i <= T; i++) {
+        if (arr[0]!=0 && i % arr[0] == 0||arr[0]==0 && i==0)
+            prev[i] = 1;
+          
+    }
+
+    for (int ind = 1; ind < n; ind++) {
+        vector<unsigned long long> cur(T + 1, 0); 
+        for (int target = 0; target <= T; target++) {
+            unsigned long long notTaken = prev[target];  
+            unsigned long long taken = 0;
+            if (arr[ind] <= target)
+                taken = cur[target - arr[ind]];  
+            cur[target] = notTaken + taken;  
+        }
+        prev = cur;  
+    }
+
+    return prev[T];
+    }
+};
