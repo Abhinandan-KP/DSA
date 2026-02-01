cabeaa421229931504ebdf070268ecfc414985a0
Abhinandan-KP <abhirop538@gmail.com>
2026-02-01 14:40:04 +0530

    Time: 15 ms (95.56%) | Memory: 9.8 MB (90.29%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-int lcs(string s,string t)
-{
-    int n=s.size();
+int minInsertions(string s) {
+       int n = s.size();
+       string t=s;
+    reverse(t.begin(),t.end());
     int m=t.size();
-    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+vector<int>prev(n+1,0),curr(m+1,0);
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
         {
             if(s[i-1]==t[j-1])
-            dp[i][j]=1+dp[i-1][j-1];
+            {
+                curr[j]=1+prev[j-1];
+            }
             else
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            {
+                curr[j]=max(prev[j],curr[j-1]);
+            }
         }
+        prev=curr;
     }
-    return dp[n][m];
-}
-    int minInsertions(string s) {
-        int n=s.size();
-        string t=s;
-        reverse(s.begin(),s.end());
-        int k=lcs(s,t);
-        return n-k;
+int k=prev[m];
+ return n-k;
     }
 };
