a9d40ae52864da723b8017bad9725cd679ee0f18
Abhinandan-KP <abhirop538@gmail.com>
2026-02-04 23:13:05 +0530

    Time: 3 ms (91.34%) | Memory: 13 MB (79.73%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
  int fun(string &s1,string &s2,int i,int j,vector<vector<int>>&dp)
  {
-      if(i<0) return j+1;
-      if(j<0) return i+1;
+      if(i==0) return j;
+      if(j==0) return i;
 
-      if(dp[i][j]!=-1)
-      return dp[i][j];
+      if(dp[i-1][j-1]!=-1)
+      return dp[i-1][j-1];
 
-      if(s1[i]==s2[j])
-      return dp[i][j]=0+fun(s1,s2,i-1,j-1,dp);
+      if(s1[i-1]==s2[j-1])
+      return dp[i-1][j-1]=0+fun(s1,s2,i-1,j-1,dp);
       else
-      return dp[i][j]=1+min(fun(s1,s2,i-1,j-1,dp),min(fun(s1,s2,i-1,j,dp),fun(s1,s2,i,j-1,dp)));
+      return dp[i-1][j-1]=1+min(fun(s1,s2,i-1,j-1,dp),min(fun(s1,s2,i-1,j,dp),fun(s1,s2,i,j-1,dp)));
  }
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return fun(s1,s2,n-1,m-1,dp);
+        return fun(s1,s2,n,m,dp);
     }
 };
