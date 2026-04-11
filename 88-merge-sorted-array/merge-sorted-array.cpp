68ec816458fb86964f93029e87d62b5d2bf58d00
Abhinandan-KP <abhirop538@gmail.com>
2026-04-11 18:51:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (99.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+// The gap starts at size n.
+// The only way the gap gets smaller is when we take a number from nums2 .
+// How many numbers are in nums2 ? Exactly n.
+// Even if we take every single number from nums2 first, the gap only shrinks n times. 
+// n-n=0
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
       int i=m-1;
       int j=n-1;
       int k=n+m-1;
