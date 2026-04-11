3629521061afb29fe7f5484dfd9d9886006d9b46
Abhinandan-KP <abhirop538@gmail.com>
2026-04-11 16:55:15 +0530

    Time: 4 ms (67.01%) | Memory: 23.7 MB (81.17%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,16 +3,18 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& A) {
         sort(A.begin(),A.end());
         vector<vector<int>>ans;
-        for(auto p:A)
-        {
-       if(ans.empty()||ans.back()[1]<p[0])
-       {
-        ans.push_back(p);
-       }
-       else
-       {
-        ans.back()[1]=max(ans.back()[1],p[1]);
-       }
+        int n=A.size();
+        for(int i=0;i<n;i++)
+        {  int start=A[i][0];
+        int end=A[i][1];
+        int j=i+1;
+            while(  j<n && A[j][0]<=end)
+            {
+              end=max(end,A[j][1]);
+              j++;
+            }
+            ans.push_back({start,end});
+            i=j-1;   // after while loop i appnd j so i have to reduce 1
         }
         return ans;
     }
