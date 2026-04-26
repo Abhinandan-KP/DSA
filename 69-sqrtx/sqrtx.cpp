3c8de5ebe9aa85a40ac918aa31ae193a1b2d2ad0
Abhinandan-KP <abhirop538@gmail.com>
2026-04-26 07:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.41%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2)  return x;
+        int lo=1,hi=x/2,ans=0;
+        while(lo<=hi)
+        {
+            long long mid=lo+(hi-lo)/2;
+            if(mid*mid <= x)
+            {
+                ans=mid;
+                lo=mid+1;
+            }
+            else
+            {
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+};
