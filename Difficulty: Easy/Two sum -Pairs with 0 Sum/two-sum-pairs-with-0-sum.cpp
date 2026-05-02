06eb4522f416c7e3f9d8d33a8a62d7528c4f6139
Abhinandan-KP <abhirop538@gmail.com>
2026-05-02 14:53:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+        vector<vector<int>> result;
+        sort(arr.begin(), arr.end());
+
+        int left = 0, right = arr.size() - 1;
+
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+
+            if(sum == 0) {
+                result.push_back({arr[left], arr[right]});
+
+                int l = arr[left], r = arr[right];
+
+                while(left < right && arr[left] == l) left++;
+                while(left < right && arr[right] == r) right--;
+            }
+            else if(sum < 0) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+};
