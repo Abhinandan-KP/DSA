9807a6320e1baa7168b36f66f45a73909ca34d2f
Abhinandan-KP <abhirop538@gmail.com>
2026-05-02 15:21:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int palPartition(string &s) {
+        int n = s.size();
+
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+                if(s[i] == s[j] && (j - i <= 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
+            }
+        }
+        vector<int> dp(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            if(isPal[0][i]) {
+                dp[i] = 0;
+            } else {
+                dp[i] = i; 
+
+                for(int j = 0; j < i; j++) {
+                    if(isPal[j + 1][i]) {
+                        dp[i] = min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
