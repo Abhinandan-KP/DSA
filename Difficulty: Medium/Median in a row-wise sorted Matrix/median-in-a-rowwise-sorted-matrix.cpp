94c205bd30712af9792e103d809b8c7372747e24
Abhinandan-KP <abhirop538@gmail.com>
2026-06-01 22:28:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,30 +1,33 @@
 class Solution {
   public:
-     int countLessEqual(vector<int>& row, int mid) {
-          return upper_bound(row.begin(), row.end(), mid) - row.begin();
+    int countlessequal(vector<int>row,int mid)
+    {
+        return upper_bound(row.begin(),row.end(),mid)-row.begin();
     }
-    int median(vector<vector<int>> &matrix) {
+    int median(vector<vector<int>> &mat) {
         // code here
-         int rows = matrix.size();
-        int cols = matrix[0].size();
-          int low = matrix[0][0];
-           int high = matrix[0][cols - 1];
-        for (int i = 1; i < rows; i++) {
-            low = min(low, matrix[i][0]);
-            high = max(high, matrix[i][cols - 1]);
+        int rows=mat.size();
+        int cols=mat[0].size();
+        int low = mat[0][0],high=mat[0][cols-1];
+        
+        for(int i=0;i<rows;i++)
+        {
+             low=min(low,mat[i][0]);
+             high=max(high,mat[i][cols-1]);
         }
-         while (low < high) {
-            int mid = (low + high) / 2;
-             int count = 0;
-            for (int i = 0; i < rows; i++) {
-                count += countLessEqual(matrix[i], mid);
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int count=0;
+            for(int i=0;i<rows;i++)
+            {
+                count+=countlessequal(mat[i],mid);
             }
-if (count < (rows * cols + 1) / 2)
-                low = mid + 1;
+            if(count<(rows*cols+1)/2)
+            low=mid+1;
             else
-                high = mid;
+            high=mid-1;
         }
-         return low;
-    
+        return low;
     }
 };
