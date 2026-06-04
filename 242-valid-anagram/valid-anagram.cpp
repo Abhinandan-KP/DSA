8196008593976d21fdcedccfdd19357eede16728
Abhinandan-KP <abhirop538@gmail.com>
2026-06-04 16:11:08 +0530

    Time: 7 ms (19.16%) | Memory: 9.7 MB (48.72%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string str1, string str2) {
+        if(str1.length()!=str2.length()) return false;
+        sort(str1.begin(),str1.end());
+        sort(str2.begin(),str2.end());
+        for(int i=0;i<str1.size();i++)
+        {
+             if(str1[i]!=str2[i]) return false;
+        }
+        return true;
+    }
+};
