1790ec07f8f4ef10075c34897b7ff32c2648e837
Abhinandan-KP <abhirop538@gmail.com>
2026-06-14 02:11:07 +0530

    Time: 75 ms (5.66%) | Memory: 86.4 MB (5.02%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,21 +10,53 @@
  */
 class Solution {
 public:
+ListNode* findMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* mergetwo(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        if (list1 != nullptr) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
+        }
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
     ListNode* sortList(ListNode* head) {
-        ListNode*temp=head;
-        vector<int>arr;
-        while(temp!=NULL)
-        {  arr.push_back(temp->val);
-            temp=temp->next;
+        if (head == nullptr || head->next == nullptr) {
+            return head;
         }
-        sort(arr.begin(),arr.end());
-        temp=head;
-        int i=0;
-        while(temp!=NULL)
-        {  temp->val=arr[i];
-            temp=temp->next;
-            i++;
-        }
-        return head;
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        ListNode* left = head;
+        
+        left = sortList(left);
+        right = sortList(right);
+        
+        return mergetwo(left, right);
     }
 };
