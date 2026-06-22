042a30df39615b05f4172bb58ac653fedf616cc8
Abhinandan-KP <abhirop538@gmail.com>
2026-06-23 00:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (60.32%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-   void fun(int ind,string s,string digits,string combos[],vector<string>&ans)
+   void fun(int ind,string digits,string s,vector<string>&ans,string box[] )
    {
-     if(ind==digits.size())
-     {
+    if(ind==digits.size())
+    {
         ans.push_back(s);
         return;
-     }
-     int digit=digits[ind]-'0';
-     for(int i=0;i<combos[digit].size();i++)
-     {
-           fun(ind+1,s+combos[digit][i],digits,combos,ans);
-     }
+    }
+    int digit=digits[ind]-'0';
+    for(int i=0;i<box[digit].size();i++)
+    {
+         fun(ind+1,digits,s+box[digit][i],ans,box);
+    }
    }
     vector<string> letterCombinations(string digits) {
-        string combos[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string>ans;
+        string box[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         string s="";
-        if(digits.empty()) return {};
-        fun(0,s,digits,combos,ans);
+        vector<string>ans;
+        fun(0,digits,s,ans,box);
         return ans;
     }
 };
