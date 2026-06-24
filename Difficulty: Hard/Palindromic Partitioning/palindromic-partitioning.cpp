8da14ad79fd0c6a1533981187d590c191ec2da75
Abhinandan-KP <abhirop538@gmail.com>
2026-06-24 18:36:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
+++ Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -1,33 +1,32 @@
 class Solution {
-  public:
+private:
+    bool isPalindrome(int i, int j, const string &s) {
+        while (i < j) {
+           if (s[i] != s[j]) return false;
+            i++;
+           j--;
+        }
+        return true;
+    }
+
+    int solve(int i, int n, const string &s, vector<int> &dp) {
+        if (i == n) return 0;
+       if (dp[i] != -1) return dp[i];
+
+        int minCost = INT_MAX;
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(i, j, s)) {
+       int cost = 1 + solve(j + 1, n, s, dp);
+        minCost = min(minCost, cost);
+            }
+        }
+   return dp[i] = minCost;
+    }
+
+public:
     int palPartition(string &s) {
-        int n = s.size();
-
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
-
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = i; j < n; j++) {
-                if(s[i] == s[j] && (j - i <= 2 || isPal[i + 1][j - 1])) {
-                    isPal[i][j] = true;
-                }
-            }
-        }
-        vector<int> dp(n, 0);
-
-        for(int i = 0; i < n; i++) {
-            if(isPal[0][i]) {
-                dp[i] = 0;
-            } else {
-                dp[i] = i; 
-
-                for(int j = 0; j < i; j++) {
-                    if(isPal[j + 1][i]) {
-                        dp[i] = min(dp[i], dp[j] + 1);
-                    }
-                }
-            }
-        }
-
-        return dp[n - 1];
+        int n = s.length();
+       vector<int> dp(n, -1);
+      return solve(0, n, s, dp) - 1;
     }
 };
