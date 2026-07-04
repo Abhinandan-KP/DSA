4634e70166649e7562bd1bbee996ac4a2931edba
Abhinandan-KP <abhirop538@gmail.com>
2026-07-04 21:00:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/XOR of a Number Range/xor-of-a-number-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public: 
+   int xortill(int n)
+   {
+       if(n%4==1) return 1;
+       if(n%4==2) return n+1;
+       if(n%4==3) return 0;
+       return n;
+   }
+    int findXOR(int l, int r) {
+        // code here
+        return xortill(l-1)^xortill(r);
+    }
+};
