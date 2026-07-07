abc62b58b6a3e35207472d6be281a8a5b2121cfb
Abhinandan-KP <abhirop538@gmail.com>
2026-07-08 01:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.09%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n<0) 
+        long long N=n;
+        if(N<0)
         {
-            x=1/x;
-
-           return  x*myPow(x,-1*(n+1));
+           x=1/x;
+           N=-N;
         }
-        if(n==0) return 1;
-        if(n==1) return x;
-       if(n%2==0)
-       {
-        return myPow(x*x,n/2);
-       }
-       return x*myPow(x,n-1);
+         double ans=1.0;
+         while(N>0)
+         {
+            if(N%2==1)
+            {
+                ans=ans*x;
+            }
+            x=x*x;
+            N/=2;
+         }
+         return ans;
     }
 };
-//Positive Range: 0 to 2,147,483,647Negative Range: -1 to -2,147,483,648Notice that there is no positive counterpart for -2,147,483,648$ inside a regular int. The positive side stops short by exactly 1.
