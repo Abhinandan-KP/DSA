cd55342bfe6560430bc3bcea851a3f3b3b23df7c
Abhinandan-KP <abhirop538@gmail.com>
2026-07-07 19:46:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/All Prime Factors in Any Order/all-prime-factors-in-any-order.cpp
+++ Difficulty: Medium/All Prime Factors in Any Order/all-prime-factors-in-any-order.cpp
@@ -1,22 +1,22 @@
 class Solution {
   public:
-    vector<int> primeFactors(int n) {
+    vector<int> primeFactors(int p) {
         // code here
         vector<int>ans;
-        while(n%2==0)
-        {
-            ans.push_back(2);
-            n/=2;
-        }
+        int n=p;
+        if(n<=1) return {};
+        
+         while(n%2==0)
+            { ans.push_back(2);
+               n/=2; 
+            }
+        
         for(int i=3;i*i<=n;i+=2)
         {
-           
-               while(n%i==0)
-               {
-                   ans.push_back(i);
-                   n/=i;
-               }
-        
+            while(n%i==0)
+            { ans.push_back(i);
+                n/=i;
+            }
         }
         if(n>1)
         {
