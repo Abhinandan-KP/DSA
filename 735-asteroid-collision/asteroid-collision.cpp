8ab0dd7e45fe4ca09ec8e4c06df3061e6459f97e
Abhinandan-KP <abhirop538@gmail.com>
2026-09-21 21:24:30 +0530

    Time: 4 ms (14.61%) | Memory: 21.7 MB (70.22%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>st;
+     for(auto ast:asteroids)
+        {   bool destroy=false;
+           while(!st.empty() && st.back()>0 && ast<0)
+            {
+                if(st.back()<abs(ast))
+                {
+                    st.pop_back();
+                    continue;
+                }
+                else if(st.back()==abs(ast))
+                {   destroy=true;
+                    st.pop_back();
+                    break;
+                }
+                else
+                {   destroy=true;
+                        break;
+                }
+            }
+            if(!destroy)
+            {
+                st.push_back(ast);
+            }
+        }
+        return st;
+    }
+};
