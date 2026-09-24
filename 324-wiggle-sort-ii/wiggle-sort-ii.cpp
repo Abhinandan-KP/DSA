d0a6db9f08b842cb293f773ffd78402cddb8e1cd
Abhinandan-KP <abhirop538@gmail.com>
2026-09-24 16:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (12.37%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& arr) {
-      sort(arr.begin(),arr.end());
-      int n=arr.size();
-      vector<int>dup1,dup2;
-       int mid=(n+1)/2;
-       for(int i=mid-1;i>=0;i--)
-       {
-             dup1.push_back(arr[i]);
-       }
-       for(int i=n-1;i>=mid;i--)
-       {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        int mid=(n+1)/2;
+        vector<int>dup1,dup2;
+        for(int i=mid-1;i>=0;i--)
+        {
+            dup1.push_back(arr[i]);
+        }
+        for(int i=n-1;i>=mid;i--)
+        {
             dup2.push_back(arr[i]);
-       }
-       int j=0;
-       for(int i=0;i<n;i+=2)
-       {
-             arr[i]=dup1[j];
-             if(i+1<n)
-             {
-                  arr[i+1]=dup2[j];
-             }
-             j++;
-       }
+        }
+        int j=0;
+        for(int i=0;i<n;i+=2)
+        {  
+            arr[i]=dup1[j];
+            if(i+1<n)
+            {
+                arr[i+1]=dup2[j];
+            }
+            j++;
+        }
     }
 };
